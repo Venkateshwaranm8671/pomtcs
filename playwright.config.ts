f9e1cd16aad10925babe0269f0ce4f6d3c98b7ee
venkateshwaran <vijayvicky0555@gmail.com>
2026-09-11 16:43:47 +0530

    done

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   reporter: [["dot"], ["json", { outputFile: "jsonReports/jsonreport.json" }], ["html", { open: "never" }]],
   use: {
     baseURL: 'https://ecommerce-playground.lambdatest.io/index.php?',
-    headless: false,
+    //headless: false,
     trace: 'on-first-retry',
     screenshot: 'on',
     video: 'on',
